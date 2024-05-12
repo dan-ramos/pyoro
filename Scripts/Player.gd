f51ac799b3a9df47fc5de17ac8837c0c5b326e78
Daniel Ramos <zespacewaffle@gmail.com>
2024-05-12 10:14:41 -0500

    tongue beta woo hoo next up is bean interactions

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,22 +1,31 @@
 extends CharacterBody3D
 
-var last_direction
+var last_direction = 1
 var tongueing = false
 var sending = true
 const SPEED = 5.0
 var tongue
 var tonguehome
 var tonguedest
+var tonguepath
+var tonguespeed = 18
+var retractspeed = 40
+
+var line_radius = 0.1
+var line_resoultion = 180
+var tonguepos = Vector3(0,0,0)
 
 func _ready():	
 	tongue = $Tongue
 	tonguehome = $"Tongue Home Marker"
 	tonguedest = $"Tongue Final Point"
+	tonguepath = $Path3D
 
 func _physics_process(delta):
-	handle_tongue()
 	if not tongueing:
 		handle_walking()
+	else:
+		handle_tongue(delta)
 
 func handle_walking():
 	if Input.is_action_just_pressed("tongue"):
@@ -28,27 +37,45 @@ func handle_walking():
 	if direction:
 		velocity.x = direction.x * SPEED
 		last_direction = direction.x
+		tonguedest.position.x = self.position.x + 40 * direction.x
+		tonguedest.position.y = self.position.y + 50 
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
 
-func handle_tongue():
-	var tonguedestpos = tongue.position 
+func handle_tongue(delta):
+	handle_middle_tongue()
 	
-	if Input.is_action_pressed("tongue") and sending:
-		tongue.position.x += .2
-		tongue.position.y += .2
+	if sending:
+		var tonguedestpos = tongue.position 
 		
-	elif Input.is_action_just_released("tongue"):
-		retract()
+		if Input.is_action_pressed("tongue") and sending:
+			#tongue.position.x += .2 * last_direction
+			#tongue.position.y += .2
+			tongue.position = tongue.position.move_toward(tonguedest.position, delta * tonguespeed)
+			
+		elif Input.is_action_just_released("tongue"):
+			sending = false
+	else:
+		retract(delta)
 
-func retract():
+func handle_middle_tongue():
+	tonguepath.curve.clear_points()
+	
+	tonguepath.curve.add_point(tongue.position * Vector3(1,1.5,1) - self.position)
+	tonguepath.curve.add_point(tonguehome.position - self.position)
+	
+
+func retract(delta):
 	sending = false
 	#print('tongue pos',tongue.position)
-	#sprint('tonguehomepos', tonguehome.position)
-	tongue.position = tonguehome.position
-	tongueing = false
+	#print('tonguehomepos', tonguehome.position)
+	tongue.position = tongue.position.move_toward(tonguehome.position, delta * retractspeed)
+	if tongue.position == tonguehome.position:
+			tongueing = false
+			tonguepath.curve.clear_points()
 
 func die():
 	print('owie pyoro not feel so good....')
